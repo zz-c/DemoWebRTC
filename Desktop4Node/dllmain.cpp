10abce7b46ae48efc6d9702b97c4248efa891413
zz <35785077@qq.com>
2022-05-24 15:44:28 +0800

    desktop4node

---
+++ Desktop4Node/dllmain.cpp
@@ -0,0 +1,37 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) int add(int a, int b) {
+    return a + b;
+};
+
+extern "C" __declspec(dllexport) int mouseMove(int x, int y) {
+    SetCursorPos(x, y);
+    return 0;
+}
+
+extern "C" __declspec(dllexport) int leftClick() {
+    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    return 0;
+}
+
+extern "C" __declspec(dllexport) int rightClick() {
+    mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+    return 0;
+}
